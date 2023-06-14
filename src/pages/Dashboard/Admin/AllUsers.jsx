f67182d23b4eefdc2e64063634f310aba2117607
Hasib <hhasibul231@gmail.com>
2023-06-15 03:44:44 +0600

    manage user complete

--- src/pages/Dashboard/Admin/AllUsers.jsx
+++ src/pages/Dashboard/Admin/AllUsers.jsx
@@ -4,7 +4,6 @@ import { FaTrashAlt, FaUserShield } from "react-icons/fa";
 import Swal from "sweetalert2";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
-
 const AllUsers = () => {
   const [axiosSecure] = useAxiosSecure();
   const { data: users = [], refetch } = useQuery(["users"], async () => {
@@ -31,27 +30,47 @@ const AllUsers = () => {
         }
       });
   };
-
-  const handleDelete = (user) => {};
+  const handleMakeInstructor = (user) => {
+    fetch(`http://localhost:5000/users/instructor/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.name} is an Instructor Now!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
 
   return (
-    <div className="w-full">
+    <div className="w-10/12">
       <Helmet>
         <title>Sports Club | Manage users</title>
       </Helmet>
-      <h3 className="text-3xl font-semibold my-4">
+      <h1 className="text-4xl text-center pb-5 my-text font-semibold">
+        Manage Users
+      </h1>
+      <h3 className="text-2xl font-semibold my-4 text-center">
         Total Users: {users.length}
       </h3>
       <div className="overflow-x-auto">
         <table className="table table-zebra w-full">
           {/* head */}
           <thead>
-            <tr>
+            <tr className="my-drawer">
               <th>#</th>
               <th>Name</th>
               <th>Email</th>
               <th>Role</th>
-              <th>Action</th>
+              <th>Role</th>
             </tr>
           </thead>
           <tbody>
@@ -61,24 +80,30 @@ const AllUsers = () => {
                 <td>{user.name}</td>
                 <td>{user.email}</td>
                 <td>
-                  {user.role === "admin" ? (
-                    "admin"
+                  {user.role === "instructor" ? (
+                    <div className="my-text font-semibold">Instructor</div>
                   ) : (
                     <button
-                      onClick={() => handleMakeAdmin(user)}
-                      className="btn btn-ghost bg-orange-600  text-white"
+                      onClick={() => handleMakeInstructor(user)}
+                      className="btn btn-ghost my-btn  text-white"
                     >
                       <FaUserShield></FaUserShield>
+                      Make Instructor
                     </button>
                   )}
                 </td>
                 <td>
-                  <button
-                    onClick={() => handleDelete(user)}
-                    className="btn btn-ghost bg-red-600  text-white"
-                  >
-                    <FaTrashAlt></FaTrashAlt>
-                  </button>
+                  {user.role === "admin" ? (
+                    <div className="my-text font-semibold">Admin</div>
+                  ) : (
+                    <button
+                      onClick={() => handleMakeAdmin(user)}
+                      className="btn btn-ghost my-btn text-white"
+                    >
+                      <FaUserShield></FaUserShield>
+                      Make Admin
+                    </button>
+                  )}
                 </td>
               </tr>
             ))}
